cdfc331a44434ff743829ee8cee6b121437e1196
juju4 <juju4@users.noreply.github.com>
2019-01-10 19:48:31 +0000

    galaxy lint, naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,11 +9,11 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-[ ! -d $rolesdir/juju4.macos-apps-install ] && git clone https://github.com/juju4/ansible-macos-apps-install $rolesdir/juju4.macos-apps-install
+[ ! -d $rolesdir/juju4.macos_apps_install ] && git clone https://github.com/juju4/ansible-macos-apps-install $rolesdir/juju4.macos_apps_install
 
 ## galaxy naming: kitchen fails to transfer symlink folder
-#[ ! -e $rolesdir/juju4.macos-yubikey ] && ln -s ansible-macos-yubikey $rolesdir/juju4.macos-yubikey
-[ ! -e $rolesdir/juju4.macos-yubikey ] && cp -R $rolesdir/ansible-macos-yubikey $rolesdir/juju4.macos-yubikey
+#[ ! -e $rolesdir/juju4.macos_yubikey ] && ln -s ansible-macos-yubikey $rolesdir/juju4.macos_yubikey
+[ ! -e $rolesdir/juju4.macos_yubikey ] && cp -R $rolesdir/ansible-macos-yubikey $rolesdir/juju4.macos_yubikey
 
 ## don't stop build on this script return code
 true
